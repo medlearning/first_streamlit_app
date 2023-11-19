f16560f3284a417638f5754c26851bbf5e93bf15
medlearning <112335231+medlearning@users.noreply.github.com>
2023-11-19 21:14:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ streamlit.dataframe(my_data_rows)
 
 fruit =  streamlit.text_input("What fruit would you like to add?",'jackfruit')
 streamlit.write('Thanks for adding ', fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
