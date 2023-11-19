3126303ff4f96426e2474dd83d963b0bc15121b3
medlearning <112335231+medlearning@users.noreply.github.com>
2023-11-19 22:20:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ except URLError as e:
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit"')")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
@@ -47,6 +47,7 @@ if streamlit.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = get_fruityvice_data(add_my_fruit)
   streamlit.text(back_from_function)
+
 
 
 #my_cur.execute("select * from fruit_load_list")
